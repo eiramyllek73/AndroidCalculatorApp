c2bd1b420078e1dcdcda33e586599687f3e10c83
Kelly McAlpine <eiramyllek73@gmail.com>
2016-02-29 14:00:02 -0500

    Initial commit

---
+++ app/src/main/java/ca/georgian/kelly/androidcalculator/MainActivity.java
@@ -0,0 +1,43 @@
+/*
+NAME:       Kelly McAlpine - 200269425
+COURSE:     COMP3025
+PROJECT:    Basic calculator for Android devices 2.3.3 and up
+DUE DATE:   March 11, 2016
+VERSION:    1.0
+ */
+
+package ca.georgian.kelly.androidcalculator;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+} //end of MainActivity
